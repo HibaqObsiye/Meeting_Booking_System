cc14798c5a82b2709cb5f7582dc44b284bf41b85
hibaq obsiye <obsiyehibaq@gmail.com>
2022-12-21 23:03:58 +0000

    refactoring tests for getPrice method

--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -8,6 +8,48 @@ describe Booking do
                 expect(subject.get_price(0)).to eq(0)
             end
         end
+
+        context 'When 60 minutes given' do
+            it 'returns 22' do
+                expect(subject.get_price(60)).to eq(22)
+            end
+        end
+
+        context 'When 1440 minutes given' do
+            it 'returns 60 ' do
+                expect(subject.get_price(1440)).to eq(60)
+            end
+        end
+
+        context 'When 10080 minutes given' do
+            it 'returns 105' do
+                expect(subject.get_price(10080)).to eq(105)
+            end
+        end
+
+        context 'When given 1.5 hours in minutes' do
+            it 'returns 1.5 hours rounded up to 2 hours ' do
+                subject.get_price(90)
+              expect(subject.hours).to eq(2)  
+            end
+        end
+
+        context 'When given 2.5 days in minutes' do
+            it 'returns 2.5 days rounded up to 3 days' do
+                subject.get_price(3600)
+              expect(subject.days).to eq(3)  
+            end
+        end
+
+        context 'When given 3.5 weeks in minutes' do
+            it 'returns 2.5 weeks rounded up to 4 weeks' do
+                subject.get_price(35280)
+              expect(subject.weeks).to eq(4)  
+            end
+        end
+
+        
+
     end
 
     # it "can make a new instance of Booking class" do
