777e3c853937a460ac90faa5fc2aef0f91b80e59
hibaq obsiye <obsiyehibaq@gmail.com>
2022-12-21 17:51:26 +0000

    initialized booking class with a time arguement that is accessible via attr_accessor

--- lib/booking.rb
+++ lib/booking.rb
@@ -1,4 +1,12 @@
 class Booking
+    attr_accessor :time, :hours, :days, :weeks
 
+    def initialize(time)
+        @time = time
+        @minute_price = 2
+        @hour_price = 22
+        @day_price = 60
+        @week_price = 105
+    end
 
 end
--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -3,7 +3,14 @@ require './lib/booking.rb'
 describe Booking do 
 
     it "can make a new instance of Booking class" do
-        expect(Booking.new).to be_an_instance_of(Booking)
+        time = 20
+        expect(Booking.new(time)).to be_an_instance_of(Booking)
     end
+
+    it "initializes with the time specified by the user" do
+        booking = Booking.new(20)
+        expect(booking.time).to eq(20)
+    end
+ 
 
 end
